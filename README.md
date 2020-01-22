f2e012e29727e1a56cd9c2ae7d8449ad8bdb92bd
Nijiko Yonskai <nijikokun@gmail.com>
2020-01-22 11:20:44 -0800

    Initial Commit :tada:

---
+++ README.md
@@ -0,0 +1,36 @@
+# Electron React Starter
+
+- 📦 Electron 7
+- 📦 React 16
+- 📦 Config (electron-store)
+- ✅ Save window location / size
+
+## Project setup
+
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+
+```
+yarn start
+```
+
+### Compiles and minifies for production
+
+```
+yarn electron-pack
+```
+
+### Tests
+
+```
+yarn test
+```
+
+### Customize configuration
+
+See [Create React App](https://github.com/facebook/create-react-app).
+
+See [Electron Builder Configuration Reference](https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/configuration.html#table-of-contents).
---
+++ public/electron.js
@@ -0,0 +1,51 @@
+const electron = require('electron');
+const windowStateKeeper = require('electron-window-state');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 900,
+    defaultHeight: 680
+  });
+
+  mainWindow = new BrowserWindow({
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindowState.manage(win);
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+
+  if (isDev) {
+    // Open the DevTools.
+    // BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
